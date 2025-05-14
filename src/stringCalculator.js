c447b8952940dee1527d28291cb560a8ade5464f
mr-rajesh <er.rajeshyadavcs@gmail.com>
2025-05-14 13:34:38 +0530

    initial logic for calculator

---
+++ src/stringCalculator.js
@@ -0,0 +1,19 @@
+function add(numbers) {
+  if (!numbers) return 0;
+
+  let delimiter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const match = numbers.match(/^\/\/(.+)\n/);
+    delimiter = new RegExp(match[1].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')); // Escape regex chars
+    numbers = numbers.slice(match[0].length);
+  }
+
+  const parts = numbers.split(delimiter).filter(n => n !== '');
+  const nums = parts.map(Number);
+
+ 
+
+  return nums.reduce((sum, n) => sum + n, 0);
+}
+
+module.exports = { add };
