58e9f5b94a8123b635621a6a1d5925eaa83c0f36
mr-rajesh <er.rajeshyadavcs@gmail.com>
2025-05-14 13:44:10 +0530

    negative calculator logic

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -10,9 +10,10 @@ function add(numbers) {
 
   const parts = numbers.split(delimiter).filter(n => n !== '');
   const nums = parts.map(Number);
-
- 
-
+  const negatives = nums.filter(n => n < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed: ${negatives.join(',')}`);
+    }
   return nums.reduce((sum, n) => sum + n, 0);
 }
 
